78849b22e3f2adef7f024496ebcd0b5b44066a44
daniel <daniel.orsted@adtraction.com>
2024-01-11 15:59:05 +0100

    Exam A1

---
+++ exam/functions.py
@@ -0,0 +1,108 @@
+import argparse
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+import matplotlib.pyplot as plt
+import numpy as np
+
+def train(epoch, model, train_loader, optimizer, loss_function, device, log_interval=10):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        recon_batch, mu, logvar = model(data)
+        loss = loss_function(recon_batch, data, mu, logvar)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.item() / len(data)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+    
+    return model
+
+
+def test(epoch, model, test_loader, loss_function, batch_size, device, name='VAE'):
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for i, (data, _) in enumerate(test_loader):
+            data = data.to(device)
+            recon_batch, mu, logvar = model(data)
+            test_loss += loss_function(recon_batch, data, mu, logvar).item()
+            if i == 0:
+                n = min(data.size(0), 8)
+                comparison = torch.cat([data[:n],
+                                      recon_batch.view(batch_size, 1, 28, 28)[:n]])
+                save_image(comparison.cpu(),
+                         'results/reconstruction_'+name+'_' + str(epoch) + '.png', nrow=n)
+
+    test_loss /= len(test_loader.dataset)
+    print('====> '+name+' Test set loss: {:.4f}'.format(test_loss))
+
+class VAE(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+
+        self.fc1 = nn.Linear(784, 400)
+        self.fc1a = nn.Linear(400, 100)
+        self.fc21 = nn.Linear(100, 2) # Latent space of 2D
+        self.fc22 = nn.Linear(100, 2) # Latent space of 2D
+        self.fc3 = nn.Linear(2, 100) # Latent space of 2D
+        self.fc3a = nn.Linear(100, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        h2 = F.relu(self.fc1a(h1))
+        return self.fc21(h2), self.fc22(h2)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        h4 = F.relu(self.fc3a(h3))
+        return torch.sigmoid(self.fc4(h4))
+
+    def forward(self, x): # when we call model(data), it is the same as model.forward(data)
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+    
+def compute_log_C(x, eps=1e-5):
+    x = torch.clamp(x, eps, 1.-eps) 
+    mask = torch.abs(x - 0.5).ge(eps)
+    far = torch.masked_select(x, mask)
+    close = torch.masked_select(x, ~mask)
+    far_values =  torch.log( (torch.log(1. - far) - torch.log(far)).div(1. - 2. * far) )
+    close_values = torch.log(torch.tensor((2.))) + torch.log(1. + torch.pow( 1. - 2. * close, 2)/3. )
+    
+    return far_values.sum() + close_values.sum()
+
+def loss_function_continuous(recon_x, x, mu, logvar):
+    # According to https://arxiv.org/pdf/1907.06845.pdf
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    
+    return BCE + KLD - compute_log_C(recon_x)
+
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    
+    return BCE + KLD
+
+def loss_function_beta(x, recon_x, mu, logvar):
+    raise NotImplementedError
